f3075aa7299d8093492f894eb6fa6e8ff24257fa
Maksymilian Malik <maksymilian.malik@gmail.com>
2023-09-28 12:44:11 +0200

    all

--- src/Card.js
+++ src/Card.js
@@ -17,13 +17,12 @@ function Card() {
       const dataWeather = await responseWeather.json()
       setWeather(dataWeather)
       setResponseText(dataWeather.message)
-      console.log(dataWeather.weather[0].icon)
       setLinkImage(`https://openweathermap.org/img/wn/${dataWeather.weather[0].icon}@2x.png`)
     }
     catch (e) {
       console.log(e)
     }
-    
+
   }
 
   const checkWeather = e => {
@@ -37,33 +36,37 @@ function Card() {
   return (
     <div className="card">
       <form className='form' onSubmit={checkWeather}>
-        <input placeholder='Enter city...' value={valueEntered || ""}  onChange={(e) => setValueEntered(e.target.value)}></input>
+        <input placeholder='Enter city...' value={valueEntered || ""} onChange={(e) => setValueEntered(e.target.value)}></input>
         <button><i className="fa-solid fa-magnifying-glass fa-beat"></i></button>
       </form>
 
 
-      {weather && weather?.cod===200 ? <div className='weatherData'>
-        <div className='weatherData_top'>
-          <img src={linkImage} className='weatherData_top_image' alt='weather'></img>
-          <div className='weatherData_top_temperatur'>{weather?.main?.temp + "°C"}</div>
-          <div className='weatherData_top_city'>{weather?.name}</div>
-        </div>
-
-        <div className='weatherData_bottom'>
-          <div className='weatherData_bottom_left'>
-            <i className="fa-solid fa-water weatherData_bottom_left_image"></i>
-            <div className='weatherData_bottom_left_text'>{weather?.main?.humidity + "%"}</div>
-            <div className='weatherData_bottom_left_humidity'>Humidity</div>
-
+      {weather && weather?.cod === 200 ?
+        <div className='weatherData'>
+          <div className='weatherData_top'>
+            <img src={linkImage} className='weatherData_top_image' alt='weather'></img>
+            <div className='weatherData_top_temperatur'>{weather?.main?.temp + "°C"}</div>
+            <div className='weatherData_top_weather'>{weather?.weather[0]?.main}</div>
+            <div className='weatherData_top_city'>{weather?.name}</div>
           </div>
 
-          <div className='weatherData_bottom_right'>
-            <i className="fa-solid fa-wind weatherData_bottom_right_image"></i>
-            <div className='weatherData_bottom_right_text'>{weather?.wind?.speed + "km/h"}</div>
-            <div className='weatherData_bottom_right_wind'>Wind speed</div>
+          <div className='weatherData_bottom'>
+            <div className='weatherData_bottom_left'>
+              <i className="fa-solid fa-water weatherData_bottom_left_image"></i>
+              <div className='weatherData_bottom_left_text'>{weather?.main?.humidity + "%"}</div>
+              <div className='weatherData_bottom_left_humidity'>Humidity</div>
+
+            </div>
+
+            <div className='weatherData_bottom_right'>
+              <i className="fa-solid fa-wind weatherData_bottom_right_image"></i>
+              <div className='weatherData_bottom_right_text'>{weather?.wind?.speed + "km/h"}</div>
+              <div className='weatherData_bottom_right_wind'>Wind speed</div>
+            </div>
           </div>
         </div>
-      </div> : <div className='noCard'>{responseText}</div>
+        :
+        <div className='noCard'>{responseText}</div>
       }
 
 
